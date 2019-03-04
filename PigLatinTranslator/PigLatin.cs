84d8cacd1c69c20c135df8418be323b7a78df856
david <david.hess681@gmail.com>
2019-03-04 15:33:07 -0500

    Properly handles punctuation appearing at the end of a string

--- PigLatinTranslator/PigLatin.cs
+++ PigLatinTranslator/PigLatin.cs
@@ -14,7 +14,7 @@ namespace PigLatinTranslator
 
         public static string Translate(string english)
         {
-            string pig = english.ToLower();
+            string pig = CutPunctuation(english.ToLower(), out string punc);
 
             // ignore if contains nums and symbols
             if (!IsFormatted(pig))
@@ -22,20 +22,12 @@ namespace PigLatinTranslator
                 return pig;
             }
 
-            char punc = english[english.Length - 1];
-            bool endsWPunc = false;
-            if (punctuation.Contains(punc))
-            {
-                endsWPunc = true;
-            }
-
             // translate based on starting character
             if (vowels.Contains(pig[0])) { pig = VowelRule(pig); }
             else { pig = ConsonantRule(pig); }
             
-            if (endsWPunc) { return pig + punc; }
 
-            return pig;
+            return pig + punc;
         }
 
         static bool IsFormatted(string word)
@@ -47,6 +39,25 @@ namespace PigLatinTranslator
             return false;
         }
 
+        static string CutPunctuation(string word, out string punc)
+        {
+            // Split ending punctuation from the word. Returns the remaining word and outs the cut punctuation.
+
+            int index = word.Length - 1;
+            punc = "";
+            while (index >= 0) 
+            {
+                if (!punctuation.Contains(word[index]))
+                {
+                    break;
+                }
+                punc = word[index] + punc;
+                index--;
+            }
+
+            return word.Substring(0, index + 1);
+        }
+
         static string VowelRule(string word)
         {
             return word + "way";
@@ -54,6 +65,11 @@ namespace PigLatinTranslator
 
         static string ConsonantRule(string word)
         {
+            if(word.Length == 1)
+            {
+                return word + "ay";
+            }
+            
             // rearrange consonants
             int wordLength = word.Length;
             for (int i = 0; i < wordLength; i++)
--- PigLatinTranslator/Program.cs
+++ PigLatinTranslator/Program.cs
@@ -10,18 +10,42 @@ namespace PigLatinTranslator
     {
         static void Main(string[] args)
         {
+            do { TranslateUserPhrase(); }
+            while (Continue());
+        }
+
+        static void TranslateUserPhrase()
+        {
             Console.Write("Welcome to the Pig Latin Translator! \nEnter a line to be translated: ");
 
             string input = Console.ReadLine();
 
             string[] parsedInput = input.Split(' ');
 
-            for(int i = 0; i < parsedInput.Length; i++)
+            for (int i = 0; i < parsedInput.Length; i++)
             {
                 Console.Write(PigLatin.Translate(parsedInput[i]) + " ");
             }
+            Console.WriteLine("\n");
+        }
 
-            Console.ReadKey();
+        static bool Continue()
+        {
+            Console.Write("Translate another line? (y/n): ");
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Y:
+                        Console.WriteLine("\n");
+                        return true;
+                    case ConsoleKey.N:
+                        return false;
+                    default:
+                        Console.Write("\nInvalid, try y/n: ");
+                        break;
+                }
+            }
         }
     }
 }
